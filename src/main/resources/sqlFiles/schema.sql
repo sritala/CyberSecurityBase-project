75b51913fc4c14b47351157c078999447ccf18b3
sritala <susanna.ritala@hotmail.com>
2019-11-29 21:52:36 +0200

    third commit

--- src/main/resources/sqlFiles/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS accounts (
-id int PRIMARY KEY auto_increment,
-username varchar(20),
-password varchar(20)
-);
-
-CREATE TABLE IF NOT EXISTS data (
-text varchar(200)
-);
